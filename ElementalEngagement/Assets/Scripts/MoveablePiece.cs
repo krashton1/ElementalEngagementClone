c0b352f843f9e6601602f6aeb7cb0713bf64b49a
EnderZone <enzo.arenas1997@gmail.com>
2020-03-02 20:39:01 -0500

    inital Unity commit

---
+++ ElementalEngagement/Assets/Scripts/MoveablePiece.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveablePiece : MonoBehaviour {
+    // Start is called before the first frame update
+    public float speed = 1.0f;
+    private NavMeshAgent nav;
+
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePiece();
+    }
+
+    public void SetFuturePosition(Vector3 V)
+    {
+        nav.destination = V;
+    }
+
+    private void MovePiece()
+    {
+    }
+}
