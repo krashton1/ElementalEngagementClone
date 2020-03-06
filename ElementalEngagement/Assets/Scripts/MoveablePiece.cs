0b2006220f89bc7daa3375bfb627e362ee6cea41
EnderZone <enzo.arenas1997@gmail.com>
2020-03-05 20:36:58 -0500

    Merge conflict 1

--- ElementalEngagement/Assets/Scripts/MoveablePiece.cs
+++ ElementalEngagement/Assets/Scripts/MoveablePiece.cs
@@ -7,19 +7,18 @@ public class MoveablePiece : Unit {
     // Start is called before the first frame update
     public float speed = 1.0f;
     public float attack_range;
+    public float stop_range;
     private NavMeshAgent nav;
     public GameObject target;
-
-
-
-
+    public GameObject projectile;
+    public int attack_frame_counter = 30;
+    public int current_attack_frame;
 	private float mElapsedTime = 0.0f;
-
 
 	void Start()
     {
         nav = GetComponent<NavMeshAgent>();
-
+        current_attack_frame = 0;
 		healthCurrent = 0;
 	}
 
@@ -27,7 +26,9 @@ public class MoveablePiece : Unit {
     void Update()
     {
         if (target != null)
+        {
             TargetEnemy();
+        }
 
 		// Time since last update, this is only here so that health can be modified over time
 		mElapsedTime += Time.deltaTime;
@@ -57,8 +58,8 @@ public class MoveablePiece : Unit {
 			}
 
 		}
-
-	}
+        AttackEnemy();
+    }
 
     public void SetFuturePosition(Vector3 V)
     {
@@ -74,7 +75,7 @@ public class MoveablePiece : Unit {
     private void TargetEnemy()
     {
         float dist = Vector3.Distance(transform.position, target.transform.position);
-        if (dist > attack_range)
+        if (dist > stop_range)
         {
             nav.destination = target.transform.position;
         }
@@ -82,6 +83,21 @@ public class MoveablePiece : Unit {
         {
             nav.destination = transform.position;
         }
+    }
 
+    private void AttackEnemy()
+    {
+        if (current_attack_frame++ > attack_frame_counter)
+        {
+            Vector3 dist = target.transform.position - transform.position;
+            if (dist.magnitude < attack_range)
+            {
+                Projectile MP = Instantiate(projectile, transform.position + (dist.normalized * 5 * GetComponent<CapsuleCollider>().radius), new Quaternion()).GetComponent<Projectile>();
+                MP.SetDirection(dist);
+                MP.SetDamage(25);
+                MP.SetGO(gameObject);
+            }
+            current_attack_frame = 0;
+        }
     }
 }
